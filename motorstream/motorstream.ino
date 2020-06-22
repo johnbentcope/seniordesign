db254f2de4eef3aa2c2edeb45cdf85f1052fcc5e
John Cope <johnbentcope@gmail.com>
2020-06-22 12:20:01 -0400

    motor control defines and some updated test code

--- motorstream/motorstream.ino
+++ motorstream/motorstream.ino
@@ -1,5 +1,5 @@
 // On my UNO, I'm using these
-#define MSHCLK 4
+#define MSHCLK 5
 #define MSHENA 6
 #define MSHDTA 7
 
@@ -9,9 +9,33 @@
 //#define MSHDTA 19
 
 #define MA_ENA 10
-#define MB_ENA 11
-#define MC_ENA 12
+#define MB_ENA 12
+#define MC_ENA 9
 #define MD_ENA 13
+
+#define MA_IN1 0x80;
+#define MA_IN2 0x40;
+#define MB_IN1 0x20;
+#define MB_IN2 0x10;
+#define MC_IN1 0x08;
+#define MC_IN2 0x04;
+#define MD_IN1 0x02;
+#define MD_IN2 0x01;
+
+#define MA_CLR (MA_IN1 | MA_IN2)
+#define MB_CLR (MB_IN1 | MB_IN2)
+#define MC_CLR (MC_IN1 | MC_IN2)
+#define MD_CLR (MD_IN1 | MD_IN2)
+
+#define MA_FWD MA_IN1
+#define MB_FWD MB_IN1
+#define MC_FWD MC_IN1
+#define MD_FWD MD_IN1
+
+#define MA_REV MA_IN2
+#define MB_REV MB_IN2
+#define MC_REV MC_IN2
+#define MD_REV MD_IN2
 
 int toggle = 0;
 char payload = 0x55;
@@ -30,12 +54,45 @@ void loop() {
 
   // Serial enable is active low
   // Hold it low while we shift in data
-  digitalWrite(MSHENA, LOW);
-  shiftOut(MSHDTA, MSHCLK, LSBFIRST, payload);
-  digitalWrite(MSHENA, HIGH);
+  payload = 1;
+  for(int i = 0; i < 8; i++){
+    digitalWrite(MSHENA, LOW);
+    shiftOut(MSHDTA, MSHCLK, LSBFIRST, payload);
+    digitalWrite(MSHENA, HIGH);
+    delay(500);
+    payload <<= 1;
+  }
 
   // tTggle the bits in payload for maximum blink
   payload ^= 0xFF;
+
+  delay(200);
+
+  for(int i = 0; i < 256; i++)
+  {
+    analogWrite(MA_ENA, i);
+    delay(5);
+  }
+  for(int i = 0; i < 256; i++)
+  {
+    analogWrite(MA_ENA, 255-i);
+    delay(1);
+  }
+  
+  delay(200);
+  
+  for(int i = 0; i < 256; i++)
+  {
+    analogWrite(MB_ENA, i);
+    delay(5);
+  }
+  for(int i = 0; i < 256; i++)
+  {
+    analogWrite(MB_ENA, 255-i);
+    delay(1);
+  }
+
+  delay(200);
 
   // Wait
   delay(1000);
